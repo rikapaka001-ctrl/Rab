36b38cfbeb794808e650288b13868c95b9b1dd2d
rikapaka001-ctrl <rikapaka001@gmail.com>
2026-05-29 12:33:37 +0530

    Update ACD_MAIN.js

--- plugins/ACD_MAIN.js
+++ plugins/ACD_MAIN.js
@@ -11,10 +11,10 @@ cmd({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const startTime = Date.now()
-        const message = await conn.sendMessage(from, { text: '*ᴀᴄᴅ-ᴍᴅ...*' })
+        const message = await conn.sendMessage(from, { text: '*𝐑ɪᴋᴀ-𝐗ᴍᴅ...❗*' })
         const endTime = Date.now()
         const ping = endTime - startTime
-        await conn.sendMessage(from, { text: `*📍 Ping : ${ping}ms*` }, { quoted: message })
+        await conn.sendMessage(from, { text: `*📍 𝐏ɪɴɢ : ${ping}ms*` }, { quoted: message })
     } catch (e) {
         console.log(e)
         reply(`${e}`)
