281e87fca33b671d7039a7dc38add8523bd6dbfc
rikapaka001-ctrl <rikapaka001@gmail.com>
2026-05-29 16:04:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = {
     PREFIX: "/",
     MODE: "public",
     AUTO_READ_STATUS: true,
+    AUTO_REACT_STATUS: false,
     AUTO_REACT: false,
     AUTO_TYPING: false,
     REACT_EMOJIS: ['❤','💕','😻','🧡','💛','💚','💙','💜','🎉','👋']
