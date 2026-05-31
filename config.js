b477f6a285901c4e58d66473f101f1d2adcca2bd
rikapaka001-ctrl <rikapaka001@gmail.com>
2026-05-31 20:38:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,71 +1,32 @@
 module.exports = {
-
-    // =========================
-    // BOT INFO
-    // =========================
-
-    BOT_NAME: "RIKA XMD",
-    OWNER_NAME: "SHAMIKA DENUWAN",
+    PREFIX: "/",
     OWNER_NUMBER: "94766619363",
+    BOT_NAME: "RIKA XMD MINI V1",
+    WORK_TYPE: "public",
 
-    // =========================
-    // PREFIX
-    // =========================
+    AUTO_TYPING: false,
+    AUTO_RECORDING: true,
+    ALWAYS_ONLINE: false,
+    ALWAYS_OFFLINE: true,
+    AUTO_READ: false,
+    READ_CMD_ONLY: true,
+    AUTO_BIO: true,
 
-    PREFIX: ".",
-
-    // =========================
-    // BOT MODE
-    // public
-    // private
-    // inbox
-    // groups
-    // =========================
-
-    MODE: "public",
-
-    // =========================
-    // AUTO FEATURES
-    // =========================
+    AUTO_REACT: false,
+    REACT_EMOJIS: ['❤️','💕','😻','🧡','💛','💚','💙','💜','🎉','👋'],
 
     AUTO_READ_STATUS: true,
-    AUTO_REACT: false,
-    AUTO_TYPING: false,
+    AUTO_VIEW_STATUS: true,
+    AUTO_LIKE_STATUS: true,
+    AUTO_LIKE_EMOJI: ['❤️', '🔥', '👍', '😂', '😮'],
 
-    // =========================
-    // REACT EMOJIS
-    // =========================
+    ANTI_BOT: false,
+    ANTI_BAD: false,
+    ANTI_LINK: false,
+    ANTI_DELETE: "true",
 
-    REACT_EMOJIS: [
-        "❤",
-        "💕",
-        "😻",
-        "🧡",
-        "💛",
-        "💚",
-        "💙",
-        "💜",
-        "🎉",
-        "👋"
-    ],
-
-    // =========================
-    // SECURITY
-    // =========================
-
-    ANTI_CALL: false,
-    AUTO_BLOCK: false,
-
-    // =========================
-    // SESSION
-    // =========================
-
-    SESSION_ID_PREFIX: "rika_",
-
-    // =========================
-    // PERFORMANCE
-    // =========================
-
-    AUTO_RECONNECT: true,
-    KEEP_ALIVE: true
+    NEWSLETTER_JID: "120363428073031350@newsletter",
+    GROUP_INVITE_LINK: "https://chat.whatsapp.com/L3uA3FqcKxU3B2FlMd52Sh",
+    MAX_RETRIES: 3,
+    RCD_IMAGE_PATH: "https://files.catbox.moe/7z5x3q.jpg"
 };
