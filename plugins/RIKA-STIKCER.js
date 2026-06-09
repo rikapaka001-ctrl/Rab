7d6826fe526c7ef2d026cdea4bc19864e8590f0a
rikapaka001-ctrl <rikapaka001@gmail.com>
2026-06-09 13:28:24 +0530

    Update RIKA-STIKCER.js

--- plugins/RIKA-STIKCER.js
+++ plugins/RIKA-STIKCER.js
@@ -36,4 +36,30 @@ async(conn, mek, m, { from, reply, quoted }) => {
         else if (mime.includes('video')) {
             const buffer = await conn.downloadMediaMessage(quoted);
             const tempPath = path.join(__dirname, `../temp/${Date.now()}.mp4`);
-            const webpPath = tempPath.replace
+            const webpPath = tempPath.replace('.mp4', '.webp');
+            
+            fs.writeFileSync(tempPath, buffer);
+            
+            // Convert video to sticker using ffmpeg
+            exec(`ffmpeg -i ${tempPath} -vcodec libwebp -fs 1M -filter:v fps=15,scale=512:512:flags=lanczos:force_original_aspect_ratio=decrease,format=rgba,pad=512:512:(ow-iw)/2:(oh-ih)/2:color=0x00000 ${webpPath}`, async (err) => {
+                if (err) {
+                    fs.unlinkSync(tempPath);
+                    return reply("*𝐄ʀᴏʀ ᴄᴏɴᴠᴇʀᴛɪɴɢ ᴠɪᴅᴇᴏ*\n\n*ᴍᴀx 6 sᴇᴄᴏɴᴅs ᴏɴʟʏ*");
+                }
+                
+                const stickerBuffer = fs.readFileSync(webpPath);
+                await conn.sendMessage(from, { sticker: stickerBuffer }, { quoted: m });
+                
+                fs.unlinkSync(tempPath);
+                fs.unlinkSync(webpPath);
+            });
+        }
+
+        else {
+            reply("*𝐎ɴʟʏ 𝐈ᴍᴀɢᴇ/𝐕ɪᴅᴇᴏ 𝐒ᴜᴘᴏʀᴛᴇᴅ*");
+        }
+    } catch (e) {
+        console.error(e);
+        reply("*𝐄ʀᴏʀ ᴄʀᴇᴀᴛɪɴɢ sᴛɪᴄᴋᴇʀ*");
+    }
+});
