a44f9bf67f686e619d2d8706495e335860149a9b
rikapaka001-ctrl <rikapaka001@gmail.com>
2026-06-12 23:54:55 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -23,7 +23,7 @@ const {
     generateWAMessageFromContent,
     generateForwardMessageContent,
     S_WHATSAPP_NET
-} = require('@whiskeysockets/baileys');
+} = require('rika-baileys');
 
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, fetchJson } = require('./lib/functions');
 const { sms } = require('./lib/msg');
