399d945e52845a7bd20d8371781081a6a9ae1e45
rikapaka001-ctrl <rikapaka001@gmail.com>
2026-06-24 10:58:31 +0530

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { cmd } = require('../command');
-const { downloadContentFromMessage } = require('@dnuzi/baileys');
+const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
 
 const dbPath = path.join(__dirname, 'vv_trigger.json');
 
