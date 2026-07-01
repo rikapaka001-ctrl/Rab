f6fdf0b4fd5b06bcf181e6b79621e74b8d5e34ee
rikapaka001-ctrl <rikapaka001@gmail.com>
2026-07-01 10:49:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
     DELETEMSGSENDTO: "owner",
         
     NEWSLETTER_JID: "120363428073031350@newsletter",
-    GROUP_INVITE_LINK: "https://chat.whatsapp.com/L3uA3FqcKxU3B2FlMd52Sh",
+    GROUP_INVITE_LINK: "https://chat.whatsapp.com/GdwUL13ZolyLW5SKYgX3D0",
     MAX_RETRIES: 3,
     RCD_IMAGE_PATH: "https://i.ibb.co/4nH9Dvch/e90b7cd6667a.jpg"
 };
